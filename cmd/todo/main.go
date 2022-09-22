e3df41b10686e545d5f0d22451bfd4d7c6b9daea
rossijonas <rossijonas@pm.me>
2022-09-22 13:53:30 -0300

    add and test cli tool

---
+++ cmd/todo/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/rossijonas/todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	// For no extra arguments, print the list
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
