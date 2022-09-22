bb329d8bce30a39de238ccdabc5f9f9fe43d73cb
rossijonas <rossijonas@pm.me>
2022-09-22 08:51:13 -0300

    todo app methods

--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -41,6 +44,44 @@ func (l *List) Complete(i int) error {
 	return nil
 }
 
-func main() {
-	fmt.Println("vim-go")
+// Delete method deletes a ToDo item from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("Item %d does not exist", i)
+	}
+
+	*l = append(ls[:i-1], ls[i:]...)
+
+	return nil
+}
+
+// Save method encodes the List as JSON and saves it
+// using the provided file name.
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, js, 0644)
+}
+
+// Get method opens the provided file name, decodes
+// the JSON data and parses it into a List
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
 }
